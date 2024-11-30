1a88a91ad7925edde77f84d0f9eeef91afe52bc9
Jihyun3478 <leejihyun3478@gmail.com>
2024-11-30 14:56:52 +0900

    feat: OutputView 작성

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -5,6 +5,22 @@ package bridge.view;
  */
 public class OutputView {
 
+    public void start() {
+        System.out.println("다리 건너기 게임을 시작합니다.");
+    }
+
+    public void inputBridgeSize() {
+        System.out.println("다리의 길이를 입력해주세요.");
+    }
+
+    public void inputMoveDirection() {
+        System.out.println("이동할 칸을 선택해주세요. (위: U, 아래: D)");
+    }
+
+    public void inputRetry() {
+        System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
+    }
+
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
@@ -19,5 +35,7 @@ public class OutputView {
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
     public void printResult() {
+        System.out.print("게임 성공 여부: ");
+        System.out.print("총 시도한 횟수: ");
     }
 }
